c76dbd8d5b525d631738053d9707d8ab87560c39
gayan5214214 <gayan@codelantic.com>
2024-06-06 16:39:56 +0530

    changes

---
+++ StudentPortal/Models/Entities/Students.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentPortal.Models.Entities
+{
+    public class Students
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+
+        public required bool Subscribed { get; set; }
+    }
+}
